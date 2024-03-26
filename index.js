5cc87b0da73f3d04a6fb617e884ea7afa85f4bba
rehemasomo <rehemasomo30@gmail.com>
2024-03-26 11:50:28 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Note: this implicitly creates a global variable
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+                           
+const leastFavoriteCustomer = 'Rehema';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Bob'; // This will result in an error because you cannot reassign a value to a constant.
+}
